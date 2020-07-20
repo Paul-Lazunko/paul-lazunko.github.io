6154bf575d179e924038606b2af33b6fa19aa107
pashaman <plavel@gmail.com>
2020-07-20 17:24:24 +0700

    initial commit

---
+++ precache-manifest.ececc0635262d11f477a2badf2a1cff8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5c10ac8cad0c73ed958",
+    "url": "/tanks/js/chunk-vendors.56768d0c.js"
+  },
+  {
+    "revision": "a1fa783cd8a8ad32f05f",
+    "url": "/tanks/js/app.6ea65770.js"
+  },
+  {
+    "revision": "d1cd153b523a6a882b86850ae3a84837",
+    "url": "/tanks/index.html"
+  },
+  {
+    "revision": "e5c10ac8cad0c73ed958",
+    "url": "/tanks/css/chunk-vendors.b208a489.css"
+  },
+  {
+    "revision": "a1fa783cd8a8ad32f05f",
+    "url": "/tanks/css/app.832cf46d.css"
+  }
+];
