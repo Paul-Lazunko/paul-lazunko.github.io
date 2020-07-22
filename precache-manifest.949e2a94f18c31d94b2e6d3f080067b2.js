f0468adf3e2cb1aee2959e71f8d5f8745a9c3cb9
pashaman <plavel@gmail.com>
2020-07-22 19:07:42 +0700

    initial commit

---
+++ precache-manifest.949e2a94f18c31d94b2e6d3f080067b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b857970925cb506f70ae",
+    "url": "/js/chunk-vendors.01c51657.js"
+  },
+  {
+    "revision": "cb3bb4202180d3adffb6",
+    "url": "/js/app.875e2486.js"
+  },
+  {
+    "revision": "cb723da0931a638a2f357d5025d5fd45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b857970925cb506f70ae",
+    "url": "/css/chunk-vendors.b208a489.css"
+  },
+  {
+    "revision": "cb3bb4202180d3adffb6",
+    "url": "/css/app.efd3c547.css"
+  }
+];
